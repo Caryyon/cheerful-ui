e9f3b288e0013f5dcfde69af562c9abb550860b8
Cary Wolff <boss@caryyon.com>
2023-05-06 23:10:51 -0500

    removes dist

--- packages/ui/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './CheerfulProvider';
-//# sourceMappingURL=index.d.ts.map
--- packages/ui/dist/utils/embeddedEntry.d.ts
+++
@@ -1,6 +0,0 @@
-import type { ReactElement } from 'react';
-import { Node } from '@contentful/rich-text-types';
-export declare const embeddedEntry: (components: {
-    [key: string]: () => ReactElement;
-}) => ({ data }: Node) => JSX.Element | null;
-//# sourceMappingURL=embeddedEntry.d.ts.map
